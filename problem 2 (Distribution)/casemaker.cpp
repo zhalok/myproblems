30e21a751757fa75d6998839f343b73f2af086dd
Zhalok <zhalokrahman007@gmail.com>
2021-01-16 00:47:56 +0600

    update

--- problem 2 (Distribution)/casemaker.cpp
+++ problem 2 (Distribution)/casemaker.cpp
@@ -1,17 +1,22 @@
 #include<bits/stdc++.h>
+#define ll long long
 using namespace std;
-#define ll long long int
 int main()
 {
-      freopen("input30.txt","w",stdout);
+   freopen("input30.txt","w",stdout);
       srand(time(NULL));
       int t=1+rand()%1000;
       cout<<t<<endl;
       for(int i=1;i<=t;i++)
       {
-          ll n=1+rand()%1000000000000000000;
-          ll m=1+rand()%1000;
-          ll h=1+rand()%1000;
-          cout<<n<<" "<<m<<" "<<h<<endl;
+          ll n=1+rand()%1000;
+          cout<<n<<endl;
+        for(int i=1;i<=n;i++)
+        {
+            ll a[i]=1+rand()%1000000000;
+            cout<<a[i]<<" ";
+        }
+        cout<<endl;
       }
 }
+
